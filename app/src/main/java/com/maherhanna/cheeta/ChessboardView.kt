9dd5e62e0d0250fe2229e309f84bbc8e2bad6888
maher-hanna <maher.hn802@gmail.com>
2021-02-22 15:58:54 +0200

    Display chessboard

---
+++ app/src/main/java/com/maherhanna/cheeta/ChessboardView.kt
@@ -0,0 +1,24 @@
+package com.maherhanna.cheeta
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+import java.util.jar.Attributes
+
+class ChessboardView(context: Context,attrs: AttributeSet): androidx.appcompat.widget.AppCompatImageView(context,attrs) {
+    lateinit var chessboardCanvas:Canvas
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val w = this.width
+        val h = this.height
+
+    }
+}
