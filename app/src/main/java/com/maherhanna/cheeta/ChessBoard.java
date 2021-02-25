ad441c96c812ec97eda3cc05b02695e4f5f9f59a
maher-hanna <maher.hn802@gmail.com>
2021-02-25 08:26:46 +0200

    Merge PlayerPiece class into Piece class
    Merge PlayerPiece class into Piece class and use ordinary memember
    variables instead of bitwise variable

--- app/src/main/java/com/maherhanna/cheeta/ChessBoard.java
+++ app/src/main/java/com/maherhanna/cheeta/ChessBoard.java
@@ -6,96 +6,82 @@ import java.util.List;
 public class ChessBoard {
     private Piece[] squares;
 
-    private List firstPlayerPieces;
-    private List secondPlayerPieces;
     private Piece.PieceColor firstPlayerColor;
 
-    public ChessBoard(Piece.PieceColor firstPlayerColor) {
-        squares= new Piece[64];
+    public static final int MIN_POSITION = 0;
+    public static final int MAX_POSITION = 63;
 
-        for(int i = 0; i < 64; ++i) {
-            squares[i] = new Piece(Piece.PieceType.EMPTY, (Piece.PieceColor)null);;
+    public ChessBoard(Piece.PieceColor firstPlayerColor) {
+        squares = new Piece[64];
+
+        for (int i = 0; i < 64; ++i) {
+            squares[i] = null;
         }
 
         this.firstPlayerColor = firstPlayerColor;
-        this.firstPlayerPieces = this.giveFirstPlayerPieces(firstPlayerColor);
-        this.secondPlayerPieces = this.giveSecondPlayerPieces(firstPlayerColor == Piece.PieceColor.WHITE ? Piece.PieceColor.BLACK : Piece.PieceColor.WHITE);
+        setUpBoard();
+    }
+
+    private void setUpBoard() {
+        setUpFirstPlayerPieces(firstPlayerColor);
+        if (firstPlayerColor == Piece.PieceColor.BLACK)
+            setUpSecondPlayerPieces(Piece.PieceColor.WHITE);
+        else
+            setUpSecondPlayerPieces(Piece.PieceColor.BLACK);
+
+
     }
 
 
-    private List giveFirstPlayerPieces(Piece.PieceColor color) {
-        ArrayList<PlayerPiece> pieces = new ArrayList();
+    private void setUpFirstPlayerPieces(Piece.PieceColor color) {
+
         int i = 0;
 
-        for(byte var4 = 7; i <= var4; ++i) {
-            this.squares[this.position(i, 1)] = new Piece(Piece.PieceType.PAWN, color);
-            pieces.add(new PlayerPiece(this.position(i, 1)));
+        for (byte var4 = 7; i <= var4; ++i) {
+            int index = getIndex(i, 1);
+            this.squares[index] = new Piece(Piece.PieceType.PAWN, color, index);
         }
 
-        this.squares[8] = new Piece(Piece.PieceType.ROOK, color);
-        pieces.add(new PlayerPiece(0));
-        this.squares[9] = new Piece(Piece.PieceType.KNIGHT, color);
-        pieces.add(new PlayerPiece(1));
-        this.squares[10] = new Piece(Piece.PieceType.BISHOP, color);
-        pieces.add(new PlayerPiece(2));
+        this.squares[8] = new Piece(Piece.PieceType.ROOK, color,0);
+        this.squares[9] = new Piece(Piece.PieceType.KNIGHT, color,1);
+        this.squares[10] = new Piece(Piece.PieceType.BISHOP, color,2);
         if (color == Piece.PieceColor.WHITE) {
-            this.squares[11] = new Piece(Piece.PieceType.QUEEN, color);
-            pieces.add(new PlayerPiece(3));
-            this.squares[12] = new Piece(Piece.PieceType.KING, color);
-            pieces.add(new PlayerPiece(4));
+            this.squares[11] = new Piece(Piece.PieceType.QUEEN, color,3);
+            this.squares[12] = new Piece(Piece.PieceType.KING, color,4);
         } else {
-            this.squares[11] = new Piece(Piece.PieceType.KING, color);
-            pieces.add(new PlayerPiece(3));
-            this.squares[12] = new Piece(Piece.PieceType.QUEEN, color);
-            pieces.add(new PlayerPiece(4));
+            this.squares[11] = new Piece(Piece.PieceType.KING, color,3);
+            this.squares[12] = new Piece(Piece.PieceType.QUEEN, color,4);
         }
 
-        this.squares[13] = new Piece(Piece.PieceType.BISHOP, color);
-        pieces.add(new PlayerPiece(5));
-        this.squares[14] = new Piece(Piece.PieceType.KNIGHT, color);
-        pieces.add(new PlayerPiece(6));
-        this.squares[15] = new Piece(Piece.PieceType.ROOK, color);
-        pieces.add(new PlayerPiece(7));
-        return pieces;
+        this.squares[13] = new Piece(Piece.PieceType.BISHOP, color,5);
+        this.squares[14] = new Piece(Piece.PieceType.KNIGHT, color,6);
+        this.squares[15] = new Piece(Piece.PieceType.ROOK, color,7);
+
     }
 
-    private final List giveSecondPlayerPieces(Piece.PieceColor color) {
-        ArrayList<PlayerPiece> pieces = new ArrayList();
+    private void setUpSecondPlayerPieces(Piece.PieceColor color) {
         int i = 0;
-
-        for(byte var4 = 7; i <= var4; ++i) {
-            this.squares[this.position(i, 6)] = new Piece(Piece.PieceType.PAWN, color);
-            pieces.add(new PlayerPiece(this.position(i, 6)));
+        for (byte var4 = 7; i <= var4; ++i) {
+            this.squares[getIndex(i, 6)] = new Piece(Piece.PieceType.PAWN, color,getIndex(i, 6));
         }
 
-        this.squares[8] = new Piece(Piece.PieceType.ROOK, color);
-        pieces.add(new PlayerPiece(this.position(0, 7)));
-        this.squares[9] = new Piece(Piece.PieceType.KNIGHT, color);
-        pieces.add(new PlayerPiece(this.position(1, 7)));
-        this.squares[10] = new Piece(Piece.PieceType.BISHOP, color);
-        pieces.add(new PlayerPiece(this.position(2, 7)));
+        this.squares[getIndex(0, 7)] = new Piece(Piece.PieceType.ROOK, color,getIndex(0, 7));
+        this.squares[getIndex(1, 7)] = new Piece(Piece.PieceType.KNIGHT, color,getIndex(1, 7));
+        this.squares[getIndex(2, 7)] = new Piece(Piece.PieceType.BISHOP, color,getIndex(2, 7));
         if (color == Piece.PieceColor.WHITE) {
-            this.squares[11] = new Piece(Piece.PieceType.KING, color);
-            pieces.add(new PlayerPiece(this.position(3, 7)));
-            this.squares[12] = new Piece(Piece.PieceType.QUEEN, color);
-            pieces.add(new PlayerPiece(this.position(4, 7)));
+            this.squares[getIndex(3, 7)] = new Piece(Piece.PieceType.KING, color,getIndex(3, 7));
+            this.squares[getIndex(4, 7)] = new Piece(Piece.PieceType.QUEEN, color,getIndex(4, 7));
         } else {
-            this.squares[11] = new Piece(Piece.PieceType.QUEEN, color);
-            pieces.add(new PlayerPiece(this.position(3, 7)));
-            this.squares[12] = new Piece(Piece.PieceType.KING, color);
-            pieces.add(new PlayerPiece(this.position(4, 7)));
+            this.squares[getIndex(3, 7)] = new Piece(Piece.PieceType.QUEEN, color,getIndex(3, 7));
+            this.squares[getIndex(4, 7)] = new Piece(Piece.PieceType.KING, color,getIndex(4, 7));
         }
 
-        this.squares[13] = new Piece(Piece.PieceType.BISHOP, color);
-        pieces.add(new PlayerPiece(this.position(5, 7)));
-        this.squares[14] = new Piece(Piece.PieceType.KNIGHT, color);
-        pieces.add(new PlayerPiece(this.position(6, 7)));
-        this.squares[15] = new Piece(Piece.PieceType.ROOK, color);
-        pieces.add(new PlayerPiece(this.position(7, 7)));
-        return pieces;
+        this.squares[getIndex(5, 7)] = new Piece(Piece.PieceType.BISHOP, color,getIndex(5, 7));
+        this.squares[getIndex(6, 7)] = new Piece(Piece.PieceType.KNIGHT, color,getIndex(6, 7));
+        this.squares[getIndex(7, 7)] = new Piece(Piece.PieceType.ROOK, color,getIndex(7, 7));
     }
 
-    public int position(int file, int rank) {
+    public int getIndex(int file, int rank) {
         return rank * 8 + file;
     }
 
--- app/src/main/java/com/maherhanna/cheeta/Piece.java
+++ app/src/main/java/com/maherhanna/cheeta/Piece.java
@@ -1,120 +1,32 @@
 package com.maherhanna.cheeta;
 
-import java.lang.Exception;
-
-
-
 
 public class Piece {
-    private byte bitValue;
+    public PieceType pieceType;
+    public PieceColor pieceColor;
+    private int position;
 
-    public Piece(PieceType pieceType, PieceColor pieceColor) {
+    public Piece(PieceType pieceType, PieceColor pieceColor,int position) {
+        this.pieceType = pieceType;
+        this.pieceColor = pieceColor;
+        setPosition(position);
 
-        this.setType(pieceType);
-        this.setColor(pieceColor);
     }
 
-    public PieceType getType() {
-        PieceType type = PieceType.EMPTY;
-        switch(this.bitValue) {
-            case 1:
-                type = PieceType.PAWN;
-            break;
-            case 2:
-                type = PieceType.ROOK;
-            break;
-            case 4:
-                type = PieceType.KNIGHT;
-            break;
-            case 8:
-                type = PieceType.BISHOP;
-            break;
-            case 16:
-                type = PieceType.QUEEN;
-            break;
-            case 32:
-                type = PieceType.KING;
-            break;
-//            default:
-//            throw new Exception("invalid internal piece bitValue");
+    public void setPosition(int newPosition){
+        if(newPosition < ChessBoard.MIN_POSITION || newPosition > ChessBoard.MAX_POSITION){
+            throw new IndexOutOfBoundsException("Trying to put a piece outside of chess board");
         }
-
-        return type;
+        this.position = newPosition;
     }
-
-    public void setType(PieceType pieceType){
-        //store the piece type and color using one byte
-        //the seven'th bit is the color
-
-        //first clear all bits but save color
-        // bit to add it after setting type bit
-        byte colorbit = (byte)(bitValue | 64);
-        bitValue = 0;
-
-        switch (pieceType){
-            case PAWN:
-                bitValue = 1;
-                break;
-            case ROOK:
-                bitValue = 2;
-                break;
-            case KNIGHT:
-                bitValue = 4;
-                break;
-            case BISHOP:
-                bitValue = 8;
-                break;
-            case QUEEN:
-                bitValue = 16;
-                break;
-        }
-
-
-        bitValue = (byte)(bitValue | colorbit);
-
-        //last bit is set to 1 to indicate that square is not empty
-        //when the bitValue is written to board
-        bitValue = (byte)(bitValue | 128);
-
-    }
-
-    public PieceColor getColor() {
-        return (this.bitValue & 64) == 1 ? PieceColor.WHITE : PieceColor.BLACK;
-    }
-
-    public void setColor(PieceColor pieceColor) {
-        if (pieceColor == PieceColor.WHITE) {
-            this.bitValue = (byte) (this.bitValue | 64);
-        } else {
-
-            this.bitValue = (byte)(this.bitValue ^ 64);
-        }
-
+    public int getPosition(){
+        return this.position;
     }
 
 
-    public enum PieceType {
-        EMPTY(0),
-        PAWN(1),
-        ROOK (2),
-        KNIGHT(4),
-        BISHOP(8),
-        QUEEN(16),
-        KING (32);
+    public enum PieceType {PAWN, ROOK, KNIGHT,BISHOP,QUEEN,KING }
 
-        private byte value;
-        PieceType(int b) {
-            this.value = (byte)b;
-        }
-
-
-    }
-
-    public enum PieceColor {
-        BLACK,
-        WHITE;
-
-    }
+    public enum PieceColor {BLACK,WHITE}
 
 }
 
