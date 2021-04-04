dbb403db405cb9696569faafc62468c740c7c8b6
maher-hanna <maher.hn802@gmail.com>
2021-04-04 14:21:27 +0300

    Run computer AI on separate thread

--- app/src/main/java/com/maherhanna/cheeta/ComputerAi.java
+++ app/src/main/java/com/maherhanna/cheeta/ComputerAi.java
@@ -4,9 +4,42 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class ComputerAi {
-    public Move getMove(ChessBoard chessBoard, Piece.Color color){
+    public Move getMove(ChessBoard chessBoard, Piece.Color toPlayNow){
+        MyRunnable myRunnable = new MyRunnable(chessBoard,toPlayNow);
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return myRunnable.getMove();
+
+
+    }
+}
+
+
+class MyRunnable implements Runnable{
+    ChessBoard chessBoard;
+    Piece.Color toPlayNow;
+    private Move move;
+
+    public MyRunnable(ChessBoard chessBoard, Piece.Color toPlayNow){
+        this.chessBoard = chessBoard;
+        this.toPlayNow = toPlayNow;
+    }
+
+    public Move getMove(){
+        return this.move;
+    }
+
+
+    @Override
+    public void run() {
         Random random = new Random();
-        ArrayList<Integer> piecesPositions = chessBoard.getPositionsFor(color);
+        ArrayList<Integer> piecesPositions = chessBoard.getPositionsFor(toPlayNow);
         int numPieces = piecesPositions.size();
         int numOfLegalMoves = 0;
         int randomPieceIndex = 0;
@@ -21,9 +54,9 @@ public class ComputerAi {
         int randomLegalMove = random.nextInt(numOfLegalMoves);
         int randomMove = chessBoard.getLegalTargetsFor(randomPiecePosition).get(randomLegalMove);
 
-        Move move = new Move(chessBoard.getPieceAt(randomPiecePosition), randomPiecePosition, randomMove);
-        move = chessBoard.getLegalMovesFor(move.getColor()).getMove(move);
-        return move;
+        Move resultMove = new Move(chessBoard.getPieceAt(randomPiecePosition), randomPiecePosition, randomMove);
+        resultMove = chessBoard.getLegalMovesFor(resultMove.getColor()).getMove(resultMove);
+        this.move = resultMove;
 
     }
 }
