c2cc5c2d8da4eb54fbb60853dd5ab262dd525f7e
Maher Hanna <maher.hn802@gmail.com>
2023-12-28 20:00:10 +0200

    Add kotlin support to project and convert State object to kotlin

---
+++ app/src/main/java/com/maherhanna/cheeta/State.kt
@@ -0,0 +1,19 @@
+package com.maherhanna.cheeta
+
+import java.util.Objects
+
+class State(
+    var allPieces: Long, @JvmField var enPassantTarget: Int, @JvmField var blackCastlingRights: Int,
+    @JvmField var whiteCastlingRights: Int
+) {
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (o == null || javaClass != o.javaClass) return false
+        val state = o as State
+        return allPieces == state.allPieces && enPassantTarget == state.enPassantTarget && blackCastlingRights == state.blackCastlingRights && whiteCastlingRights == state.whiteCastlingRights
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(allPieces, enPassantTarget, blackCastlingRights, whiteCastlingRights)
+    }
+}
