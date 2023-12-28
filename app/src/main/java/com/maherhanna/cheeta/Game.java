47550f7180ce7b8630b7910879dc5d0ada699c69
Maher Hanna <maher.hn802@gmail.com>
2023-12-28 13:16:49 +0200

    Upgrade gradle from 7.0 to 7.2

--- app/src/main/java/com/maherhanna/cheeta/Game.java
+++ app/src/main/java/com/maherhanna/cheeta/Game.java
@@ -10,7 +10,7 @@ class Game {
     private static final String trickyPosition = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ";
     private static final String killerPosition = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
     private static final String cmkPosition = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ";
-    private static final String positionInUse = killerPosition;
+    private static final String positionInUse = startPosition;
     public static final long COMPUTER_MAX_SEARCH_TIME = 4;
 
     private final Drawing drawing;
