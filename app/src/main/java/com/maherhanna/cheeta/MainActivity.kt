b629fd573ca97e9f51438b824c1f3f448d898095
Maher Hanna <maher.hn802@gmail.com>
2024-02-05 16:50:01 +0300

    Add adMob to the game

--- app/src/main/java/com/maherhanna/cheeta/MainActivity.kt
+++ app/src/main/java/com/maherhanna/cheeta/MainActivity.kt
@@ -2,18 +2,73 @@ package com.maherhanna.cheeta
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.maherhanna.cheeta.core.Piece
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn_start: Button
     var rb_white: RadioButton? = null
     lateinit var rb_black: RadioButton
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) {  }
+
+        var adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.toString())
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
+
+        mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.")
+            }
+
+            override fun onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                Log.d(TAG, "Ad dismissed fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.")
+                mInterstitialAd = null
+            }
+
+            override fun onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.")
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.")
+            }
+        }
         supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
         btn_start = findViewById<Button>(R.id.btn_start)
@@ -24,6 +79,11 @@ class MainActivity : AppCompatActivity() {
             var humanPiecesColor = Piece.WHITE
             if (rb_black.isChecked()) humanPiecesColor = Piece.BLACK
             gameIntent.putExtra("humanPiecesColor", humanPiecesColor)
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            }
             startActivity(gameIntent)
         })
     }
